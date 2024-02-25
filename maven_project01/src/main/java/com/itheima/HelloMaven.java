3986670b10c4b9e23982759316d40668cd006662
Grace <Grace@atguigu.com>
2024-02-25 13:52:26 +0800

    first commit

---
+++ maven_project01/src/main/java/com/itheima/HelloMaven.java
@@ -0,0 +1,7 @@
+package com.itheima;
+
+public class HelloMaven {
+    public static void main(String[] args) {
+        System.out.println("Maven 项目创建成功！");
+    }
+}
---
+++ maven_project01/src/test/java/com/itheima/DemoTest.java
@@ -0,0 +1,19 @@
+package com.itheima;
+import org.junit.Test;
+
+public class DemoTest {
+    @Test
+    public void test1() {
+        System.out.println("Test1 成功执行");
+    }
+
+    @Test
+    public void test2() {
+        System.out.println("Test2 成功执行");
+    }
+
+    @Test
+    public void test3() {
+        System.out.println("Test3 成功执行");
+    }
+}
