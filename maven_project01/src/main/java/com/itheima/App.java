3869a48ae2f0bf5eba3ea9d6cc3af34a904d7bc5
Grace <Grace@atguigu.com>
2024-02-25 14:16:20 +0800

    hot-fix commit1

--- maven_project01/src/main/java/com/itheima/App.java
+++ maven_project01/src/main/java/com/itheima/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World! Be Brave!" );
+        System.out.println("Hello hot-fix 1");
     }
 }
